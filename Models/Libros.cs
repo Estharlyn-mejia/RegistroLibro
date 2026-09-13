2ec65c16229302f9704ff23990d7d10041d95f11
Estharlyn Mejia Regalado <estharlyn_mejia@ucne.edu.do>
2026-09-13 13:25:04 -0400

    Tarea1 aplicada1

---
+++ Models/Libros.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroLibro.Models;
+
+public class Libros
+{
+    [Key]
+    public int LibroId { get; set; }
+
+    public string Titulo { get; set; } = string.Empty;
+
+    public string Autor { get; set; } = string.Empty;
+
+    public int AnoPublicacion { get; set; }
+}
