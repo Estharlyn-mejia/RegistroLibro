c61a1c74df984a6f9b558b964f7c3661d16a336a
Estharlyn-mejia <estharlyn_mejia@ucne.edu.do>
2026-09-21 20:42:54 -0400

    Revert "Revert "Practica en clase""

---
+++ RegistroLibro.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroLibro.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var a = 10; var b = 20;
+        var resultado = a + b;
+        Assert.Equal(30, resultado);
+    }
+}
---
+++ RegistroLibro/Context/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroLibro.Models;
+
+namespace RegistroLibro.Context;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Libros> Libros { get; set; }
+
+    public DbSet<Estudiantes> Estudiantes{get; set;}
+}
